630e50a74126214e8cc58bb0bb1b282cf7128ec2
Ricardo Rios <ricardojrios@outlook.com>
2024-09-10 10:21:01 -0400

    erased data in the timesheet template

--- pathvalidity.py
+++ pathvalidity.py
@@ -1,10 +1,14 @@
 from pathlib import Path
 import os
 
-p = Path('../Timesheet4Escalate/TimesheetTemplate')
+p = Path('../Timesheet4Escalate/TimesheetTemplate.csv')
 
 if p.exists():
     print('Yes the path exists')
+    file = open('../Timesheet4Escalate/TimesheetTemplate.csv', 'r')
+    content = file.read()
+    print(content)
+    
 
 else:
     print('THe path doesnt exist')
@@ -12,4 +16,9 @@ else:
 
 
 print(os.getcwd())
+
+
+
+
+op = open('../Timesheet4Escalate/TimesheetTemplate.csv')
 
